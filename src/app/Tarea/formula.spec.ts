3dabccd46af3a8b4a3cf4113eb0f0b089c8e299a
asunawesker <zS19004913@estudiantes.uv.mx>
2021-10-15 01:48:24 -0500

    adding more test

--- src/app/Tarea/formula.spec.ts
+++ src/app/Tarea/formula.spec.ts
@@ -7,14 +7,47 @@ describe('FormulaPuntoMedio', () => {
         formula = new Punto();
       });
     
-    it('should increment totalVotes when upvoted', () => {
-    formula.puntoMedio(3, 9, -1, 5);
+    it('should show the middle point (1,7)', () => {
+        formula.puntoMedio(3, 9, -1, 5);
+        
+        let puntoEsperado = {
+            x: 1,
+            y: 7,
+        };
+
+        expect(formula.punto).toEqual(puntoEsperado);
+    });   
     
-    let puntoEsperado = {
-        x: 1,
-        y: 7,
-    }
-    
-    expect(formula.punto).toEqual(puntoEsperado);
-    });    
+    it('the middle point should be on the origin', () => {
+        formula.puntoMedio(7, 12, -7, -12);
+
+        let puntoEsperado = {
+            x: 0,
+            y: 0,
+        };
+
+        expect(formula.punto).toEqual(puntoEsperado);
+    });
+
+    it('abscissa should be on the origin', () => {
+        formula.puntoMedio(1, 2,-1, 2);
+
+        let puntoEsperado = {
+            x: 0,
+            y: 2,
+        };
+
+        expect(formula.punto).toEqual(puntoEsperado);
+    });
+
+    it('ordinate should be on the origin', () => {
+        formula.puntoMedio(5, 2, 5, -2);
+
+        let puntoEsperado = {
+            x: 5,
+            y: 0,
+        };
+
+        expect(formula.punto).toEqual(puntoEsperado);
+    });
 })
